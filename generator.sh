678542e02058fdeea76a28338da6f1a4d79431fa
Marc93z <marc-39@outlook.fr>
2025-03-12 20:13:58 +0100

    ajstement sript

--- generator.sh
+++ generator.sh
@@ -1,109 +1,115 @@
-#!/bin/bash 
+#!/bin/bash
 
 #####################################
 ##
-##     Genere un docker-compose 
+##     Génère un docker-compose 
 ##
 #####################################
 
-########## Variable ############
+########## Variables ############
 
 DIR="${HOME}/generator"
-USER_Script=${USER} 
+USER_SCRIPT=${USER}
 
-########## Fonction ############
+########## Fonctions ############
 
 help() {
-
-echo "Usage : 
-
-    ${0##*/} [-h ] [--help]
-
-Options:
--h , --help : aide
--p , --postgres : lance une instance une postgres
--i , --ip : affichage des ip
- "    
+    echo "Usage : 
+    ${0##*/} [-h|--help] [-p|--postgres] [-i|--ip]"
+    
+    echo "Options:
+    -h, --help : aide
+    -p, --postgres : lance une instance PostgreSQL
+    -i, --ip : affichage des IP"
 }
 
 ip() {
-
- for i in $(docker ps -q);do docker inspect -f "{{range.NetworkSettings.Network}} {{.IPAdress}}{{end}} -  {{.Name}} $i "  ;done 
-
-    }
-
-parser_option() {
-
-case $@ in 
-        
-        -h | --help )
-        help
-        ;;
-        -p | --postgres ) 
-        postgres
-        ;;
-        *) 
-       echo "option invalide, lancer -help ou -h"      
-esac  
+    for i in $(docker ps -q); do
+        docker inspect -f "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} - {{.Name}} $i"
+    done
 }
 
 postgres() {
+    echo ""
+    echo "Installation d'une instance PostgreSQL..."
+    echo ""
 
-echo""
-echo "Installation d une instance..."
-echo""
-echo "1- Creation du repertoir de data..."
+    # Création du répertoire de données
+    mkdir -p "$DIR"
 
-mkdir -p $DIR 
-echo""
-echo"
- version: 27.5.1
- service: 
-    postgress:
-        image: postgress: latest
-        container_name: postgress
-        environment: 
-        -POSTGRES_USER : myuser
-        -POSTGRES_PASSWRD : passwd 
-        -POSTGRES_DATABASE : mydb 
-        -ports:
-        -5332:5332
-        volumes: 
-        - postgres_data:var/lib/postgres
+    # Génération du fichier docker-compose
+    cat > "$DIR/docker-compose-postgres.yml" <<EOF
+version: '3.9'
+services:
+    postgres:
+        image: postgres:latest
+        container_name: postgres
+        environment:
+            - POSTGRES_USER=myuser
+            - POSTGRES_PASSWORD=passwd
+            - POSTGRES_DB=mydb
+        ports:
+            - "5332:5432"
+        volumes:
+            - postgres_data:/var/lib/postgresql/data
         networks:
-        -generator
-    Volumes:
-        postgres_data:
+            - generator
+
+volumes:
+    postgres_data:
         driver: local
         driver_opts:
-        o: bind
-        type : none 
-        device : ${DIR}/postgres
-    newtorks:
-        generator:
-            driver:bridge
-            ipam:
-                config: 
-                    subnet : 192.169.0.0/24
- 
- "$DIR>docker-compose-postgres.yml
- 
- echo "2- Run de l instance postgres"
-     docker-compose -f  $DIR>docker-compose-postgres.yml up -d
-echo""
-echo "
-Crendantials :
--Port :5332:5332
--POSTGRES_USER : myuser
--POSTGRES_PASSWRD : passwd 
--POSTGRE_DATA : mydb
+            type: none
+            device: ${DIR}/postgres
+            o: bind
 
-Command : psql -h  <ip> -u myuser -d mydb
-" 
- }  
+networks:
+    generator:
+        driver: bridge
+        ipam:
+            config:
+                - subnet: 192.168.0.0/24
+EOF
 
+    # Lancement de l'instance PostgreSQL
+    echo "2- Run de l'instance PostgreSQL"
+    docker-compose -f "$DIR/docker-compose-postgres.yml" up -d
 
-########## Execute #############
+    echo ""
+    echo "Crédentials :
+    - Port : 5332
+    - POSTGRES_USER : myuser
+    - POSTGRES_PASSWORD : passwd 
+    - POSTGRES_DB : mydb
 
-parser_option $@
-ip
+    Commande : psql -h localhost -p 5332 -U myuser -d mydb"
+}
+
+parser_option() {
+    while  [[ $# -gt 0 ]]; do
+        case $1 in
+            -h | --help )
+                help
+                exit 0
+                ;;
+            -p | --postgres ) 
+                postgres
+                exit 10 # Sortie avec code 10 pour le lancement de l'instance PostgreSQL
+                ;;
+            -i | --ip )
+                ip
+                exit 20 # Sortie avec code 20 pour l'affichage des IP
+                ;;
+            * )
+                echo "Option invalide, lancer -h ou --help"
+                exit 1 # Sortie avec code 1 pour une option invalide
+                ;;
+        esac
+        shift
+    done
+}
+
+########## Exécution #############
+
+parser_option "$@"
+
